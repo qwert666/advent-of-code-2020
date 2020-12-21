68f39920dcd4798633e2b201771ef8c4ccaf1ca6
Michal Datberg <michal.datberg@axelspringer.com>
2020-12-22 00:23:10 +0100

    day3 python

---
+++ day_03/solution.py
@@ -0,0 +1,14 @@
+f = open("input.txt", "r")
+input_data = f.readlines()
+count = len(input_data)
+
+check_index = [(3*i % 31)+1 for i in range(1, count)]
+
+result = 0
+for (index, line) in enumerate(input_data):
+    if index > 0:
+        cond = line[check_index[index - 1 ] - 1]
+        if cond == "#":
+            result += 1
+
+print(result)
